f8aeb96c486a80d534cf7c3cbd6ff8324be20445
Rodrigo Cabrera Castaldoni <castaldoniro@gmail.com>
2022-01-06 22:48:02 -0300

    adiciona custom dataset

---
+++ networks/custom_dataset.py
@@ -0,0 +1,24 @@
+from torch.utils.data import Dataset
+import numpy as np
+
+
+class BeansDataset(Dataset):
+    def __init__(self, X: np.array, y: np.array, transform=None):
+
+        self.X = X.astype("float32")
+        self.y = y.astype("float32")
+
+        self.n_samples = self.X.shape[0]
+        self.transform = transform
+
+    def __len__(self):
+        return self.n_samples
+
+    def __getitem__(self, idx):
+
+        sample = self.X[idx], self.y[idx]
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
